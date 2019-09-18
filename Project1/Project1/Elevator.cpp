b9068497a222019f1f56b06997dff2640466de91
Thomas Armenta <tja7w6@mail.umkc.edu>
2019-09-17 22:18:29 -0500

    Added some commints

--- Project1/Project1/Elevator.cpp
+++ Project1/Project1/Elevator.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main()
 {
-	//Start of project so far empty but on gitub and canvas :D
+	
+
+	// how an elevator handles and keeps track of requests made by users. 
+	// all requests users make are stored
+
+	// A user pushes either the up or down button to request the elevator.
+
+
+	//Once the elevator arrives, the user gets in and specifies his/her destination floor.
+	//The elevator prioritizes the requests that are on the way where it’s going, but also based on a first come first served principle
+
+	//Meanwhile, other users outside or inside the elevator make requests as well. The elevator stores the requests and handles them efficiently
 
 	return 0;
 }
